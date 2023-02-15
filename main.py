7793468bb8007cecdaf05be0cca9b02f4bdcedc4
Santiago Gonzalez <parrasanti.gonzalez@gmail.com>
2023-02-15 13:59:10 -0500

    Se leyo el inicio rapido de flask

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for, request, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -15,4 +15,27 @@ def hello():
 @app.route("/<name>")
 def hello_world(name):
     return f"Hello, {escape(name)}"
+    
+#peticiones HTTP en una sola ruta
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return 'YA INGRESASTE INFO'
+    else:
+        return 'APENAS QUIERES INGRESAR INFO'
+
+# gerera un template y se retornan los parametros en render template
+@app.route("/generar_template/")
+@app.route("/generar_template/<name>")
+def generar_template(name=None):
+    return render_template('hello.html', name=name)
+
+
+#Prueba las direcciones relativas con url_for
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('hello'))
+    print(url_for('static', filename='static_css/style.css'))
+
+
 
